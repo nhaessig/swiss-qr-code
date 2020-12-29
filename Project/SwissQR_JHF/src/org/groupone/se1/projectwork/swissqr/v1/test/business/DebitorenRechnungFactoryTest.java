564d031d80d4ba726d5bdacd7476b464b369dda7
MarkusKaufmann <Markus.Kaufmann1@gmx.ch>
2020-12-29 13:42:33 +0100

    Adding base for tests

---
+++ Project/SwissQR_JHF/src/org/groupone/se1/projectwork/swissqr/v1/test/business/DebitorenRechnungFactoryTest.java
@@ -0,0 +1,5 @@
+package org.groupone.se1.projectwork.swissqr.v1.test.business;
+
+public class DebitorenRechnungFactoryTest {
+
+}
---
+++ Project/SwissQR_JHF/src/org/groupone/se1/projectwork/swissqr/v1/test/persistence/DebitorenRechnungDAOTest.java
@@ -0,0 +1,5 @@
+package org.groupone.se1.projectwork.swissqr.v1.test.persistence;
+
+public class DebitorenRechnungDAOTest {
+
+}
