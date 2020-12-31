d2d61624bff3c6e3ccf28b5aa6d3ee05eec69313
MarkusKaufmann <Markus.Kaufmann1@gmx.ch>
2020-12-31 10:22:26 +0100

    Refactoring and beautifying main of swissqrmanager App

--- Project/SwissQR_JHF/src/ch/groupone/swissqr/v1/presentation/ConsoleClient.java
+++ Project/SwissQR_JHF/src/ch/groupone/swissqr/v1/presentation/ConsoleClient.java
@@ -6,8 +6,10 @@ import ch.groupone.swissqr.v1.business.DebitorenRechnung;
 import ch.groupone.swissqr.v1.business.DebitorenRechnungFactory;
 
 /**
- * Class implementing a console client as main in-/output for the SwissQRRechnungsManagerApp
- * Please note that all accounting specific terms are written in German because it is an exclusive project for accounting in Switzerland
+ * Class implementing a console client as main in-/output for the
+ * SwissQRRechnungsManagerApp Please note that all accounting specific terms are
+ * written in German because it is an exclusive project for accounting in
+ * Switzerland
  * 
  * @author Fabrizio Piacente, Markus Kaufmann, Nicolas Hässig
  */
@@ -16,15 +18,16 @@ public class ConsoleClient {
 	// Singleton DebitorenFactory to use
 	private DebitorenRechnungFactory debitorenRechnungFactory = null;
 
-	/** 
+	/**
 	 * Default Constructor for th Console Client
 	 */
 	public ConsoleClient() {
 		debitorenRechnungFactory = DebitorenRechnungFactory.getInstance();
 	}
 
-	/** 
+	/**
 	 * Creates a new DebitorenRechnung and saves it with the listet attributes
+	 * 
 	 * @param double betrag
 	 * @param String zahlungsempfaenger
 	 * @param String zahlungspflichtiger
@@ -37,16 +40,17 @@ public class ConsoleClient {
 				zahlungsempfaenger, zahlungspflichtiger, iban));
 	}
 
-	/** 
+	/**
 	 * Deletes the DebitorenRechnung with the given id
+	 * 
 	 * @param int debitorenRechnungID
 	 */
 	public void deleteDebitorenRechnung(int debitorenRechnungID) {
 
 		debitorenRechnungFactory.deleteDebitorenRechnung(debitorenRechnungID);
 	}
-	
-	/** 
+
+	/**
 	 * Deletes all existing debitorenRechnungen
 	 */
 	public void deleteAllDebitorenRechnung() {
@@ -54,19 +58,35 @@ public class ConsoleClient {
 		debitorenRechnungFactory.deleteAllDebitorenRechnung();
 	}
 
-	/** 
+	/**
 	 * Helper function to display a single Debitorenrechnung on the console
+	 * 
 	 * @param DebitorenRechnung debitorenRechnung
 	 */
 	public void displayDebitorRechnungOnConsole(DebitorenRechnung debitorenRechnung) {
-		System.out.println("---DebitorenRechnung:---ID:" + debitorenRechnung.debitorenRechnungID());
-		System.out.println("\tBetrag: " + String.format("%.2f", debitorenRechnung.getBetrag()) + " CHF");
-		System.out.println("\tZahlungsempfaenger: " + debitorenRechnung.getZahlungsempfaenger());
-		System.out.println("\tZahlungspflichtiger: " + debitorenRechnung.getZahlungspflichtiger());
+
+		try {
+			System.out.println("---DebitorenRechnung:---ID:" + debitorenRechnung.debitorenRechnungID());
+			System.out.println("\tBetrag: " + String.format("%.2f", debitorenRechnung.getBetrag()) + " CHF");
+			System.out.println("\tZahlungsempfaenger: " + debitorenRechnung.getZahlungsempfaenger());
+			System.out.println("\tZahlungspflichtiger: " + debitorenRechnung.getZahlungspflichtiger());
+
+		} catch (Exception e) {
+			System.out.println("Invalid or nonexisting debitorrechnung");
+		}
+
 	}
 
-	/** 
+	/**
+	 * Helper function to show a separator
+	 */
+	public void displaySeperator() {
+		System.out.println("-------------------------------------------------------------------------------------");
+	}
+
+	/**
 	 * Displays the DebitorenRechnung with the given id
+	 * 
 	 * @param int debitorenRechnungID
 	 */
 	public void displayDebitorenRechnung(int debitorenRechnungID) {
@@ -76,7 +96,7 @@ public class ConsoleClient {
 		displayDebitorRechnungOnConsole(debitorenRechnung);
 	}
 
-	/** 
+	/**
 	 * Displays all existing DebitorenRechnungen
 	 */
 	public void displayAllDebitorRechnungen() {
--- Project/SwissQR_JHF/src/ch/groupone/swissqr/v1/presentation/SwissQRRechnungsManagerApp.java
+++ Project/SwissQR_JHF/src/ch/groupone/swissqr/v1/presentation/SwissQRRechnungsManagerApp.java
@@ -1,43 +1,71 @@
 package ch.groupone.swissqr.v1.presentation;
 
+import java.util.Random;
+
 /**
- * Class containing the main function for the SwissQRRechnungsManagerApp
- * Please note that all accounting specific terms are written in German because it is an exclusive project for accounting in Switzerland
+ * Class containing the main function for the SwissQRRechnungsManagerApp Please
+ * note that all accounting specific terms are written in German because it is
+ * an exclusive project for accounting in Switzerland
  * 
  * @author Fabrizio Piacente, Markus Kaufmann, Nicolas Hässig
  */
 public class SwissQRRechnungsManagerApp {
 
-	/** 
+	// Some test data for showcase
+	private static final String[] zahlungsempfaengerOrPflichtigerArray = { "MonoAg", "Rudolf Meier", "Six Finance",
+			"Treuhand +", "Las Miranda" };
+	private static final double[] betragArray = { 100.0, 55.55, 32.15, 10.25, 5.00 };
+	private static final String[] qrIbanArray = { "CH4431999123000889012", "CH4231998124010859023",
+			"CH4131998124010159011", "CH3131928164016159016 +", "CH2139998124010159019" };
+
+	// function to generate and save test data
+	private static void generateTestData(int amount, ConsoleClient consoleClient) {
+		Random rand = new Random();
+		
+		// please note that all test arrays mus have the same size
+		int testarraysize = zahlungsempfaengerOrPflichtigerArray.length;
+		
+		for (int i = 0; i < amount; i++) {
+
+			consoleClient.createDebitorenRechnung(betragArray[rand.nextInt(testarraysize)],
+					zahlungsempfaengerOrPflichtigerArray[rand.nextInt(testarraysize)],
+					zahlungsempfaengerOrPflichtigerArray[rand.nextInt(testarraysize)], qrIbanArray[rand.nextInt(testarraysize)]);
+		}
+	}
+
+	/**
 	 * Main method of the SwissQRRechnungsManagerApp
+	 * 
 	 * @param String[] args
 	 */
 	public static void main(String[] args) {
 
 		ConsoleClient consoleClient = new ConsoleClient();
-
-		consoleClient.createDebitorenRechnung(120.25, "MonoAg", "Hans Meier", "CH4431999123000889012");
-		consoleClient.createDebitorenRechnung(10.50, "MonoAg", "Six Fiance", "CH4231998124010859023");
-
+		
+		// Console Client with testdata
+		generateTestData(5, consoleClient);
+		
 		consoleClient.displayAllDebitorRechnungen();
-
-		consoleClient.deleteDebitorenRechnung(2);
-
-		consoleClient.createDebitorenRechnung(23.00, "MonoAg", "Treuhand +", "CH4131998124010159011");
-
+		consoleClient.displaySeperator();
+		
 		consoleClient.displayDebitorenRechnung(3);
+		consoleClient.displaySeperator();
+		
+		consoleClient.deleteDebitorenRechnung(3);
+		
+		consoleClient.displayAllDebitorRechnungen();
+		consoleClient.displaySeperator();
+
+		generateTestData(1, consoleClient);
 		consoleClient.displayAllDebitorRechnungen();
 		
+		consoleClient.displaySeperator();
 		consoleClient.deleteAllDebitorenRechnung();
-		
-		consoleClient.createDebitorenRechnung(120.25, "MonoAg", "Hans Meier", "CH4431999123000889012");
-		consoleClient.createDebitorenRechnung(10.50, "MonoAg", "Six Fiance", "CH4231998124010859023");
-		consoleClient.deleteAllDebitorenRechnung();
-		
-		
-		consoleClient.createDebitorenRechnung(120.25, "MonoAg", "Hans Meier", "CH4431999123000889012");
-		consoleClient.createDebitorenRechnung(10.50, "MonoAg", "Six Fiance", "CH4231998124010859023");
-		consoleClient.displayAllDebitorRechnungen();
+		consoleClient.displaySeperator();
+
+		consoleClient.displaySeperator();
+		consoleClient.displayDebitorenRechnung(1);
+		consoleClient.displaySeperator();
 	}
 
 }
